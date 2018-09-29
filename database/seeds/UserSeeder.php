7dfee13ff8b9714562ef908d6cb0a5e14147ff12
kevinangkajaya <kevinangkajaya.gukguk@gmail.com>
2018-09-29 20:32:48 +0700

    commit all 2

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        for($i=0;$i<10;$i++) {
+            DB::table('users')->insert([
+                //'name' => str_random(5),
+                'name' => $faker->name,
+                'age' => rand(17,30),
+                'address' => $faker->address,
+                'hobby' => $faker->jobTitle
+            ]);
+        }
+    }
+}
