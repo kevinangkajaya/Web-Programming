28f6819202aeed63028ff9fee400875d1b29c8a4
sapibesar <yudigondrong15@yahoo.co.id>
2018-12-06 16:14:53 +0700

    haha

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -13,33 +13,24 @@
     <form method="POST" action="/register" enctype="multipart/form-data">
         {{csrf_field()}}        
         <p class="labels">Full Name </p>
-        <input type="text" name="username" placeholder="Full Name"><br>
-
+        <input type="text" name="fullName" placeholder="Full Name"><br>
         <p class="labels">Email </p>
-        <input type="email" name="useremail" placeholder="Email"><br>
-
+        <input type="email" name="userEmail" placeholder="Email"><br>
         <p class="labels">Password </p>
-        <input type="password" name="userpassword" placeholder="Password"><br>
-
+        <input type="password" name="password" placeholder="Password"><br>
         <p class="labels">Confirm Password </p>
-        <input type="password" name="userconfirmpass" placeholder="Confirm Password"><br>
-
+        <input type="password" name="password_confirmation" placeholder="Password"><br>
         <p class="labels">Phone </p>
-        <input type="text" name="userphone" placeholder="Phone"><br>
-
+        <input type="text" name="userPhone" placeholder="Phone"><br>
         <p class="labels">Category Gender </p>
-        <select name="usergender">
+        <select name="userGender">
             <option value="">Select Gender</option>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
         </select> <br>
-
         <p class="labels">Address </p>
-        <textarea rows="3" type="text" name="useraddress" placeholder="Address"></textarea><br>
-        
+        <textarea rows="3" type="text" name="userAddress" placeholder="Address"></textarea><br>
         <input type="file" name="userPfp"><br>
-
         <input type="submit" value="Register">
-
     </form>
 @endsection
