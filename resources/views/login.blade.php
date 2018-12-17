9a868ecd06d80691eb4226bdd1b3ba8d65ea1266
kevinangkajaya <kevinangkajaya@yahoo.com>
2018-12-17 22:03:30 +0700

    tambah cookie

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,9 +13,9 @@
     <form method="POST" action="/login">
         {{csrf_field()}}
         <p class="labels">Email address </p>
-        <input type="email" name="email"><br>
+    <input type="email" name="email" value="{{Cookie::get("email")}}"><br>
         <p class="labels">password </p>
-        <input type="password" name="password"><br>
+    <input type="password" name="password" value="{{Cookie::get("password")}}"><br>
         <input type="checkbox" name="checkbox" style="width:unset;">Remember Me<br>
         
         <input type="submit" value="Login">
